0ac16d1e03451d9dc1505d55241af627a877fe1a
Elf Sundae <elf.sundae@gmail.com>
2019-06-24 17:27:42 +0800

    Rename github to manager

--- Example/iOS Example/ViewController.m
+++ Example/iOS Example/ViewController.m
@@ -25,28 +25,28 @@
     [AFNetworkReachabilityManager.sharedManager startMonitoring];
 
     NSURL *baseURL = [NSURL URLWithString:@"https://api.github.com"];
-    AFHTTPSessionManager *github = [[AFHTTPSessionManager alloc] initWithBaseURL:baseURL];
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:baseURL];
 
 #if DEBUG
-    github.logger.enabled = YES;
-    [github.logger setLogLevel:AFLoggerLevelDebug];
+    manager.logger.enabled = YES;
+    [manager.logger setLogLevel:AFLoggerLevelDebug];
 #endif
 
-    github.requestSerializer.HTTPRequestHeadersBlock = ^NSDictionary<NSString *, id> * _Nullable (NSURLRequest * _Nonnull request, id _Nullable parameters) {
+    manager.requestSerializer.HTTPRequestHeadersBlock = ^NSDictionary<NSString *, id> * _Nullable (NSURLRequest * _Nonnull request, id _Nullable parameters) {
         return @{ @"FooHeader": NSUUID.UUID.UUIDString };
     };
-    github.requestSerializer.URLQueryParametersBlock = ^NSDictionary<NSString *, id> * _Nullable (NSString * _Nonnull method, NSString * _Nonnull URLString, id _Nullable parameters) {
+    manager.requestSerializer.URLQueryParametersBlock = ^NSDictionary<NSString *, id> * _Nullable (NSString * _Nonnull method, NSString * _Nonnull URLString, id _Nullable parameters) {
         return @{ @"_time": @((long)NSDate.date.timeIntervalSince1970) };
     };
 
-    [github GET:@"users/ElfSundae" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id _Nullable responseObject) {
+    [manager GET:@"users/ElfSundae" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id _Nullable responseObject) {
         // NSLog(@"responseObject: %@", responseObject);
     } failure:nil];
 
     // Should not log
-    [github GET:@"https://httpbin.org/get" parameters:nil progress:nil success:nil failure:nil];
+    [manager GET:@"https://httpbin.org/get" parameters:nil progress:nil success:nil failure:nil];
 
-    [github GET:@"/users/ElfSundae/repos"
+    [manager GET:@"/users/ElfSundae/repos"
      parameters:@{ @"type": @"all", @"sort": @"updated" }
        progress:nil success:nil failure:nil];
 }
